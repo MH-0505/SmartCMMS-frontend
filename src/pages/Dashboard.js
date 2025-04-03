c32b09284468b129e5f6d97b4976fa0918186f72
michał <272980@student.pwr.edu>
2025-04-04 01:10:33 +0200

    dodano obsługę godziny rozpoczęcia/zakończenia

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -81,8 +81,23 @@ function NewMaintenanceForm({ onClose }) {
 }
 
 function NewFailureReportForm({ onClose }) {
-    function submitFailureReport() {
+    const [startTime, setStartTime] = useState("--:--");
+    const [endTime, setEndTime] = useState("--:--");
+    const [startWholeDay, setStartWholeDay] = useState(false);
+    const [endWholeDay, setEndWholeDay] = useState(false);
 
+    function handleStartTimeCheckbox(checked){
+        setStartWholeDay(checked)
+        setStartTime("--:--");
+    }
+
+    function handleEndTimeCheckbox(checked){
+        setEndWholeDay(checked)
+        setEndTime("--:--");
+    }
+
+    function submitFailureReport() {
+        // TODO
     }
 
     return (
@@ -105,7 +120,7 @@ function NewFailureReportForm({ onClose }) {
                         <option>Inne</option>
                     </select>
 
-                    <label htmlFor={"technician"}>Wykonawca:</label>
+                    <label htmlFor={"technician"}>Wykonawcy:</label>
                     <select name={"technician"} id={"technician"}>
                         <option>Jan Kowalski</option>
                         <option>Adam Nowak</option>
@@ -113,16 +128,46 @@ function NewFailureReportForm({ onClose }) {
                     </select>
 
                     <label htmlFor={"description"}>Opis:</label>
-                    <textarea name={"description"} id={"description"} rows={5} style={{resize: "vertical"}}/>
+                    <textarea name={"description"} id={"description"} rows={8} style={{resize: "vertical"}}/>
 
-                    <label htmlFor={"startDate"}>Planowana data rozpoczęcia:</label>
-                    <input type={"date"} name={"startDate"} id={"startDate"} style={{minWidth:"fit-content", width:"120px"}}/>
+                    <label htmlFor={"startDate"}>Planowany termin rozpoczęcia:</label>
+                    <div>
+                        <input type={"date"} name={"startDate"} id={"startDate"}
+                               style={{ minWidth: "fit-content", width: "120px", height: "100%"}}/>
+                        <label style={{ marginLeft: "10px"}}>
+                            Godzina:
+                            <input type={"time"} name={"startTime"} value={startTime} disabled={startWholeDay}
+                                   onChange={e => setStartTime(e.target.value)}
+                                   style={{ marginLeft: "10px"}}/>
+                        </label>
+                        <label style={{ marginLeft: "10px"}}>
+                            Cały dzień -
+                            <input type={"checkbox"} name={"startWholeDay"} checked={startWholeDay}
+                                   onChange={e => handleStartTimeCheckbox(e.target.checked)}
+                                   style={{marginLeft: "5px"}}/>
+                        </label>
+                    </div>
 
-                    <label htmlFor={"endDate"}>Planowana data zakończenia:</label>
-                    <input type={"date"} name={"endDate"} id={"endDate"} style={{minWidth:"fit-content", width:"120px"}}/>
+                    <label htmlFor={"endDate"}>Planowany termin zakończenia:</label>
+                    <div>
+                        <input type={"date"} name={"endDate"} id={"endDate"}
+                               style={{minWidth: "fit-content", width: "120px", height: "100%"}}/>
+                        <label style={{marginLeft: "10px"}}>
+                            Godzina:
+                            <input type={"time"} name={"endTime"} value={endTime} disabled={endWholeDay}
+                                   onChange={e => setEndTime(e.target.value)}
+                                   style={{marginLeft: "10px"}}/>
+                        </label>
+                        <label style={{marginLeft: "10px"}}>
+                            Cały dzień -
+                            <input type={"checkbox"} name={"endWholeDay"} checked={endWholeDay}
+                                   onChange={e => handleEndTimeCheckbox(e.target.checked)}
+                                   style={{marginLeft: "5px"}}/>
+                        </label>
+                    </div>
 
                     <label htmlFor={"priority"}>Priorytet:</label>
-                    <select name={"priority"} id={"priority"} style={{minWidth:"fit-content", width:"120px"}}>
+                    <select name={"priority"} id={"priority"} style={{minWidth: "fit-content", width: "120px"}}>
                         <option>Bardzo wysoki</option>
                         <option>Wysoki</option>
                         <option>Normalny</option>
@@ -131,7 +176,9 @@ function NewFailureReportForm({ onClose }) {
                     </select>
                 </div>
 
-                <button type="submit" style={{marginTop:"auto", justifySelf:"right"}}>Zatwierdź zgłoszenie</button>
+                <button type="submit" className="Apply-button" style={{marginTop:"auto", alignSelf:"end"}}>
+                    Zatwierdź zgłoszenie
+                </button>
             </form>
         </>
     );
